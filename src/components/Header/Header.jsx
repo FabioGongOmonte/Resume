8e3994aaf738a885caaa76c19944016b8d46433a
FabioGongOmonte <fabiogongomonte@gmail.com>
2023-02-14 13:34:14 -0500

    Changes to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-import css from '../../styles/index.css'
+import css from './Header.module.scss'
+import {BiPhoneCall} from 'react-icons/bi'
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className={'paddings ${css.wrapper}'}>
+            
+            <div className={'flexCenter innerWidth ${css.container}'}>
+                <div className={css.name}>
+                    Fabio
+                </div>
+
+                <ul className={'flexCenter ${css.menu}'}>
+                    <li><a href="">Education</a></li>
+                    <li><a href="">Experience</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Technical Skills</a></li>
+                    <li>
+                    <p>+1 514 318 0820</p>
+                    <BiPhoneCall size={"40px"}/>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
