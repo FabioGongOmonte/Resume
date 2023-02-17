503615c5fd2fc48ed27d3cdbdde9af96d1536962
FabioGongOmonte <fabiogongomonte@gmail.com>
2023-02-17 13:10:54 -0500

    changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,58 @@
-import React from 'react'
-import css from './Header.module.scss'
-import {BiPhoneCall} from 'react-icons/bi'
+import React, { useEffect, useRef, useState } from "react";
+import css from "./Header.module.scss";
+import { BiPhoneCall, BiMenuAltRight } from "react-icons/bi";
+import { motion } from "framer-motion";
+import { getMenuStyles, headerVariants } from "../../utils/motion";
+import useOutsideAlerter from "../../hooks/useOutsideAlerter";
+import useHeaderShadow from "../../hooks/useHeaderShadow";
+
 const Header = () => {
-    return (
-        <div className={'paddings ${css.wrapper}'}>
-            
-            <div className={'flexCenter innerWidth ${css.container}'}>
-                <div className={css.name}>
-                    Fabio
-                </div>
+  const menuRef = useRef(null);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerShadow = useHeaderShadow();
 
-                <ul className={'flexCenter ${css.menu}'}>
-                    <li><a href="">Education</a></li>
-                    <li><a href="">Experience</a></li>
-                    <li><a href="">Projects</a></li>
-                    <li><a href="">Technical Skills</a></li>
-                    <li>
-                    <p>+1 514 318 0820</p>
-                    <BiPhoneCall size={"40px"}/>
-                    </li>
-                </ul>
-            </div>
+  //to handle click outside of sidebar on mobile
+  useOutsideAlerter({
+    menuRef,
+    setMenuOpened,
+  });
 
+  return (
+    <motion.div
+      variants={headerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`bg-primary paddings ${css.wrapper}`}
+      viewport={{ once: true, amount: 0.25 }}
+      style={{boxShadow: headerShadow}}
+    >
+      <div className={`innerWidth ${css.container} flexCenter`}>
+        <div className={css.name}>Fabio</div>
+        <ul
+          className={`flexCenter ${css.menu}`}
+          ref={menuRef}
+          style={getMenuStyles(menuOpened)}
+        >
+          <li><a href="#experties">Education</a></li>
+          <li><a href="#work">Experience</a></li>
+          <li><a href="#portfolio">Projects</a></li>
+          <li><a href="#people">Technical Skills</a></li>
+          <li className={`flexCenter ${css.phone}`}>
+            <p>+1 (514) 318 0820</p>
+            <BiPhoneCall size={"40px"} />
+          </li>
+        </ul>
+
+        {/* for medium and small screens */}
+        <div
+          className={css.menuIcon}
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
         </div>
-    )
-}
+      </div>
+    </motion.div>
+  );
+};
 
-
-export default Header
+export default Header;
